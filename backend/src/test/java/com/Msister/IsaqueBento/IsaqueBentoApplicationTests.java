77e25ae8c9e1381abda18d94c0092e7c6ce13abf
ErickBento10 <erickbento@yahoo.com.br>
2022-05-11 09:30:22 -0300

    projeto estruturado

---
+++ backend/src/test/java/com/Msister/IsaqueBento/IsaqueBentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Msister.IsaqueBento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsaqueBentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
