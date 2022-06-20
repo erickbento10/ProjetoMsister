624659a059931a96c608ab74a9ce5e8520e9d801
ErickBento10 <erickbento@yahoo.com.br>
2022-06-20 12:02:42 -0300

    Criação do componete home

--- frontEnd/src/app/app.routing.ts
+++ frontEnd/src/app/app.routing.ts
@@ -3,9 +3,11 @@ import {Routes, RouterModule} from '@angular/Router';
 
 import {PhotosComponent} from './photos/photos.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home/home.component';
 
 const APP_ROUTES: Routes = [ 
-{path: "Home", component:AppComponent} ,   
+{path: "", component:AppComponent},     
+{path: "home", component:HomeComponent} ,   
 { path: "fotos", component: PhotosComponent},
 
 ];
