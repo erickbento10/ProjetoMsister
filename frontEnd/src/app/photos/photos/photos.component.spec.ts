6720e33a20b34d6c935918b79888b06bd2fcedef
ErickBento10 <erickbento@yahoo.com.br>
2022-06-26 10:03:21 -0300

    acrencentou mais uma linha de fotos

--- frontEnd/src/app/photos/photos/photos.component.spec.ts
+++ frontEnd/src/app/photos/photos/photos.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PhotosComponent } from './photos.component';
+import { PhotosComponent } from '../photos.component';
 
 describe('PhotosComponent', () => {
   let component: PhotosComponent;
